66a6336507ab2e5a9acf79b127344572cdf8d4f5
Joanne <jeffiefeifei@gmail.com>
2017-07-11 21:12:09 -0700

    [clean] code

--- server/serverHelpers.js
+++ server/serverHelpers.js
@@ -1,21 +1,20 @@
 const spotifyHelper = require("./spotifyApiHelpers");
 const lyr = require('lyrics-fetcher');
-var Sequelize = require('sequelize');
-var Songs = require('../db/schema').Songs;
+const Sequelize = require('sequelize');
+const Songs = require('../db/schema').Songs;
 
 module.exports = {
   getSpotifyData: function(req, res) {
     const spotifyApi = spotifyHelper.getSpotifyAPI();
     spotifyApi.searchTracks(req.body.string)
       .then(function(data) {
-        console.log(data.body.tracks.items[0].album.images[2].url);
-        var name = data.body.tracks.items[0].name;
-        var artist = data.body.tracks.items[0].artists[0].name;
-        var pic = data.body.tracks.items[0].album.images[2].url;
+        let name = data.body.tracks.items[0].name;
+        let artist = data.body.tracks.items[0].artists[0].name;
+        let pic = data.body.tracks.items[0].album.images[2].url;
         Songs.find({ where: { songName: name, artistName: artist, url: pic } })
           .then(function (song) {
             if (song) {
-              var newViews = song.dataValues.views + 1;
+              let newViews = song.dataValues.views + 1;
               song.update({ views: newViews });
             } else {
               Songs.create({
@@ -29,84 +28,72 @@ module.exports = {
         res.status(data.statusCode).send(data.body);
       }, function(err) {
         res.send(err.statusCode, err);
-      }, function(err) {
-        console.error(err);
       });
   },
-
   getMostPopular: function(req, res) {
     Songs.findAll({ limit: 10, order: 'views DESC' })
       .then(function(data) {
-        res.status(200);
-        res.send(data);
+        res.send(200, data);
       }, function(err) {
-        console.error(err);
+        res.send(err.statusCode, err);
       });
   },
-
-  getArtistTopTracks: function (req, res) {
+  getArtistTopTracks: function(req, res) {
     const spotifyApi = spotifyHelper.getSpotifyAPI();
     spotifyApi.searchTracks(req.body.string)
       .then(function(data) {
-        var artistId = data.body.tracks.items[0].artists[0].id;
+        let artistId = data.body.tracks.items[0].artists[0].id;
         return spotifyApi.getArtistTopTracks(artistId, 'US')
           .then(function(tracks) {
-           res.status(tracks.statusCode).send(tracks.body);
+            res.send(tracks.statusCode, tracks.body);
           }, function(err) {
             res.send(err.statusCode, err);
           });
       }, function(err) {
-        console.error(err);
+        res.send(err.statusCode, err);
       });
   },
-
-  getArtistAlbums: function (req, res) {
+  getArtistAlbums: function(req, res) {
     const spotifyApi = spotifyHelper.getSpotifyAPI();
     spotifyApi.searchTracks(req.body.string)
       .then(function(data) {
-        var artistId = data.body.tracks.items[0].artists[0].id;
+        let artistId = data.body.tracks.items[0].artists[0].id;
         return spotifyApi.getArtistAlbums(artistId)
           .then(function(albums) {
-            res.send(data.statusCode, albums.body);
+            res.send(albums.statusCode, albums.body);
           }, function(err) {
             res.send(err.statusCode, err);
           });
       }, function(err) {
-        console.error(err);
+        res.send(err.statusCode, err);
       });
   },
-
-  getArtistInfo: function (req, res) {
+  getArtistInfo: function(req, res) {
     const spotifyApi = spotifyHelper.getSpotifyAPI();
     spotifyApi.getArtist(req.body.id)
       .then(function(data) {
         res.send(data.statusCode, data.body);
       }, function(err) {
         res.send(err.statusCode, err);
-      }, function(err) {
-        console.error(err);
       });
   },
-
-  getAlbumInfo: function (req, res) {
+  getAlbumInfo: function(req, res) {
     const spotifyApi = spotifyHelper.getSpotifyAPI();
     spotifyApi.getAlbums([req.body.id])
       .then(function(data) {
         res.send(data.statusCode, data.body);
       }, function(err) {
         res.send(err.statusCode, err);
-      }, function(err) {
-        console.error(err);
       });
   },
-
   getRelated: function(req, res) {
     return new Promise(function (resolve, reject) {
       const spotifyApi = spotifyHelper.getSpotifyAPI();
-      return spotifyApi.getArtistRelatedArtists(req.body.artistId).then(function (data) {
-        data.body.artists.sort(function (a, b) {
-          return b.popularity - a.popularity;
-        });
+      return spotifyApi.getArtistRelatedArtists(req.body.artistId)
+        .then(function(data) {
+          data.body.artists.sort(function(a, b) {
+            return b.popularity - a.popularity;
+          });
         data.body.artists = data.body.artists.filter(function (artist) {
           return req.body.excludeList.indexOf(artist.id) === -1;
         });
@@ -115,11 +102,10 @@ module.exports = {
       });
     });
   },
-
   getLyricsDetail: function (req, res) {
-    var artist = req.body.artist;
-    var track = req.body.track;
-    lyr.fetch(artist, track, function (err, lyrics) {
+    let artist = req.body.artist;
+    let track = req.body.track;
+    lyr.fetch(artist, track, function(err, lyrics) {
       if (err) {
         res.send(err.statusCode, err);
       }
