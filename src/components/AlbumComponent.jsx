e68725cec3f232d9f2e1729c90f5cd09eda94f4d
Joanne <jeffiefeifei@gmail.com>
2017-07-11 21:53:17 -0700

    [clean] albumComponent

--- src/components/AlbumComponent.jsx
+++ src/components/AlbumComponent.jsx
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { artistAlbums } from '../modules/ajax';
 import { albumInfo } from '../modules/ajax';
-
 import Map from './MapComponent';
 
 class Album extends Component {
   constructor(props) {
     super(props);
-    this.state = { album: [], videoId: this.props.currentSong.videoId }
+    this.state = { album: [], videoId: this.props.currentSong.videoId };
   }
 
   componentDidMount() {
@@ -22,38 +21,36 @@ class Album extends Component {
   }
 
   displayAlbums() {
-    var that = this;
-    var uniqAlbums;
-    var newAlbumList
+    let that = this;
+    let uniqAlbums;
+    let newAlbumList;
     artistAlbums(this.props.currentSong.artistName, function(data) {
-      var obj = {};
-      for (var i = 0; i < data.items.length; i++) {
-        var item = data.items[i].name;
+      let obj = {};
+      for (let i = 0; i < data.items.length; i++) {
+        let item = data.items[i].name;
         if (!obj[item]) {
           obj[item] = data.items[i];
         }
       }
 
-      var uniqAlbumsName = Object.keys(obj);
+      let uniqAlbumsName = Object.keys(obj);
       uniqAlbums = [];
-      for (var i = 0; i < uniqAlbumsName.length; i++) {
+      for (let i = 0; i < uniqAlbumsName.length; i++) {
         uniqAlbums.push(obj[uniqAlbumsName[i]]);
       }
 
-      var listOfAlbumIds = [];
+      let listOfAlbumIds = [];
       newAlbumList = [];
-      for (var i = 0; i<uniqAlbums.length; i++) {
+      for (let i = 0; i<uniqAlbums.length; i++) {
         listOfAlbumIds.push(uniqAlbums[i].id);
       }
       albumInfo(listOfAlbumIds).then(function (data) {
-        for (var j = 0; j<data.albums.length; j++) {
+        for (let j = 0; j<data.albums.length; j++) {
           newAlbumList.push(data.albums[j])
         }
       that.setState({ album: newAlbumList, videoId: that.props.currentSong.videoId });
       });
-
     });
-
   }
 
   render() {
@@ -65,36 +62,36 @@ class Album extends Component {
     return(
       <div className ="row">
         <div className ="col-xs-12 col-md-6">
-        <div className='mapHeader'>Available Spotify Markets</div>
+          <div className='mapHeader'>Available Spotify Markets</div>
           <Map/>
         </div>
-       <div className ="col-xs-6 col-md-5">
+        <div className ="col-xs-6 col-md-5">
 
           <div className='albumsHeader'>Artist's Albums</div>
-        <div className= 'genAlbums'>
-          {this.state.album.map(function(album, index){
-            if (album.album_type === "album") {
-              return (
-                <div className = 'eachAlbum' key={index}>
-                  <br/>
-                  <img id="albumImg" src={album.images[1].url}></img>
-                  <div className='description'>
-                    <div id="albumName">{album.name}</div>
+          <div className= 'genAlbums'>
+            {this.state.album.map(function(album, index){
+              if (album.album_type === "album") {
+                return (
+                  <div className = 'eachAlbum' key={index}>
                     <br/>
-                    <div id="albumTracks">{album.tracks.total} tracks</div>
+                    <img id="albumImg" src={album.images[1].url}></img>
+                    <div className='description'>
+                      <div id="albumName">{album.name}</div>
+                      <br/>
+                      <div id="albumTracks">{album.tracks.total} tracks</div>
+                      <br/>
+                      <div id="albumRelease"><p>Release date: </p>{album.release_date}</div>
+                      <br/>
+                      <div id="albumCopyright">{album.copyrights[0].text.slice(0, 110).replace(/\w+[.!?]?$/, '')}</div>
+                    </div>
                     <br/>
-                    <div id="albumRelease"><p>Release date: </p>{album.release_date}</div>
-                    <br/>
-                    <div id="albumCopyright">{album.copyrights[0].text.slice(0, 110).replace(/\w+[.!?]?$/, '')}</div>
-                  </div>
-                  <br/>
 
-                </div>
-              );
-            }
-          }, this)}
+                  </div>
+                );
+              }
+            }, this)}
+          </div>
         </div>
-      </div>
       </div>
     );
   }
