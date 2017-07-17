255a54d9582b8bb502f7c361b12b44533f8775bd
Joanne <jeffiefeifei@gmail.com>
2017-07-17 16:23:48 -0700

    [fix] access token retry

--- server/spotifyApiHelpers.js
+++ server/spotifyApiHelpers.js
@@ -5,16 +5,26 @@ const spotifyApi = new SpotifyWebApi(keys.spotify);
 module.exports = {
   getAccessToken: function() {
     // Retrieve an access token
-    spotifyApi.clientCredentialsGrant()
-      .then(function(data) {
-        console.log('The access token expires in ' + data.body['expires_in']);
-        console.log('The access token is ' + data.body['access_token']);
+    const grantMe = () => {
+      const p = new Promise((resolve, reject) => {
+        spotifyApi.clientCredentialsGrant()
+          .then(function(data) {
+            console.log('The access token expires in ' + data.body['expires_in']);
+            console.log('The access token is ' + data.body['access_token']);
 
-        // Save the access token so that it's used in future calls
-        spotifyApi.setAccessToken(data.body['access_token']);
-      }, function(err) {
-        console.log('Something went wrong when retrieving an access token', err.message);
-      });
+            // Save the access token so that it's used in future calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+          }, function(err) {
+            console.log('Something went wrong when retrieving an access token', err.message);
+            throw err;
+          });
+        });
+      return p;
+    };
+
+    grantMe()
+    .catch((e) => { grantMe(); });
+
   },
   getSpotifyAPI: function() {
     this.getAccessToken();
